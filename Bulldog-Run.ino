343653ca1a86c9b2a10cd849ac3809855abd1aa6
Addie Elwood <elwoodadriana@gmail.com>
2019-04-25 15:06:27 -0400

    Moved to PROGMEM

--- Bulldog-Run.ino
+++ Bulldog-Run.ino
@@ -1,7 +1,8 @@
 // All characters are fixed as a 5x7 matrix.
-#include <string.h>
+// #include <string.h>
+// #include "font5x7.h"
 
-#include "font5x7.h"
+#include <avr/pgmspace.h>
 #include <RGBmatrixPanel.h>
 
 #define CLK  8
@@ -26,6 +27,7 @@ uint16_t color;
 #define MAX_OBSTACLE_CYCLE (6)
 int MAX_OBSTACLE_INDEX;
 
+// 9 bytes
 typedef struct gameState {
   char* disp_mat;
   int bulldog_altitude;
@@ -35,7 +37,8 @@ typedef struct gameState {
 } gameState;
 
 // +1 because extra space needed for string null-terminator.
-char bulldog[BULLDOG_SIZE][BULLDOG_SIZE + 1] = {
+// 56 bytes
+const char bulldog[BULLDOG_SIZE][BULLDOG_SIZE + 1] PROGMEM = {
   "__W___W",
   "___WWW_",
   "___WWW_",
@@ -45,7 +48,8 @@ char bulldog[BULLDOG_SIZE][BULLDOG_SIZE + 1] = {
   "__W_W__"
 };
 
-unsigned char LetterFont[] = {
+// 130 bytes
+const unsigned char LetterFont[] PROGMEM = {
   0x7E, 0x11, 0x11, 0x11, 0x7E,// A
   0x7F, 0x49, 0x49, 0x49, 0x36,// B
   0x3E, 0x41, 0x41, 0x41, 0x22,// C
@@ -74,28 +78,64 @@ unsigned char LetterFont[] = {
   0x61, 0x51, 0x49, 0x45, 0x43,// Z
 };
 
-int bulldog_jump_heights[JUMP_SEQUENCE_LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-char* obstacles = (char*)
-"______HELLO______";
+// 38 bytes
+// calculate this instead
+//int bulldog_jump_heights[JUMP_SEQUENCE_LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+const char obstacles[] PROGMEM =
+"_HELLO______";
 int obstacle_index; // Current character at the bottom left corner.
 int obstacle_cycle; // Each character takes up 5 columns, so it takes 5 cycles to move 1 character over.
 
 unsigned long previous_millis;
 unsigned long current_millis;
-unsigned long delay_millis = 250;
+unsigned long delay_millis = 200;
 
 gameState* my_game;
 int cur_jumping_idx = 0;
 
+
+//size_t mystrlen(const char *str)
+//{
+//    size_t len;
+//    for (len = 0; str[len]; len++);
+//    return len;
+//}
+
+/* This function places the current value of the heap and stack pointers in the
+ * variables. You can call it from any place in your code and save the data for
+ * outputting or displaying later. This allows you to check at different parts of
+ * your program flow.
+ * The stack pointer starts at the top of RAM and grows downwards. The heap pointer
+ * starts just above the static variables etc. and grows upwards. SP should always
+ * be larger than HP or you'll be in big trouble! The smaller the gap, the more
+ * careful you need to be. Julian Gall 6-Feb-2009.
+ */
+int * heapptr, * stackptr;
+void check_mem() {
+  stackptr = (int *)malloc(4);          // use stackptr temporarily
+  heapptr = stackptr;                     // save value of heap pointer
+  free(stackptr);      // free up the memory again (sets stackptr to 0)
+  stackptr =  (int *)(SP);           // save value of stack pointer
+
+  Serial.print(F("SP: ")); Serial.print((unsigned int) stackptr); Serial.print(F(" > HP: "));
+  Serial.println((unsigned int) heapptr);
+}
+
 void setup() {
-  // For LED Matrix.
-  matrix.begin();
-  
   // For Serial Monitor.
   Serial.begin(9600);
-  unsigned char column_bitmap = LetterFont[(('A' - 'A') * 5) + 1];
-  Serial.println(column_bitmap);
-//  Serial.println("hi");
+ 
+  // For LED Matrix.
+  matrix.begin();
+
+  check_mem(); 
+  
+  unsigned char column_bitmap = (unsigned char) pgm_read_byte(&(LetterFont[(('A' - 'A') * 5) + 0]));
+
+  Serial.println(column_bitmap, HEX);
+  //Serial.println(column_bitmap);
+  // Serial.println("hi");
 
 //  // Initialize a new game.
   my_game = (gameState*) malloc(sizeof(gameState));
@@ -111,7 +151,7 @@ void setup() {
   
   obstacle_index = 0;
   obstacle_cycle = 0;
-  MAX_OBSTACLE_INDEX = strlen(obstacles) - 6;
+  MAX_OBSTACLE_INDEX = strlen_P(obstacles) - 6;
   
   previous_millis = millis();
 
@@ -121,20 +161,24 @@ void setup() {
   matrix.drawPixel(matrix.height()-1, 0, matrix.Color333(0, 0, 7));
   fillLEDLetters();
   fillLEDBulldog();
+
+  check_mem();
 }
 
 void loop() {
-//  unsigned long current_millis = millis();
-//  if (current_millis - previous_millis >= delay_millis) {
-//    int read_val = Serial.read();
-////    clearDisplayMat();
-//    clearLEDBoard();
-//    // When spacebar is pressed.
-//    if (read_val == 10) {
-//      my_game->bulldog_jumping = true;
-//    }
+  unsigned long current_millis = millis();
+  if (current_millis - previous_millis >= delay_millis) {
+    int read_val = Serial.read();
+    Serial.println(read_val);
+//    clearDisplayMat();
+    // clearLEDBoard();
+    // When spacebar is pressed.
+    if (read_val == 'A') {
+      my_game->bulldog_jumping = true;
+      my_game->bulldog_altitude = 8;
+    }
 //    int char_under_bulldog_index = obstacle_index + (int) ((BULLDOG_LEFT_OFFSET + BULLDOG_SIZE - BULLDOG_FRONT_EAR_OFFSET + obstacle_cycle) / 6);
-//    int char_under_bulldog = obstacles[char_under_bulldog_index];
+//    unsigned char char_under_bulldog = (unsigned char) pgm_read_byte(&(obstacles[char_under_bulldog_index]));
 //    if (char_under_bulldog != '_') {
 //      if (my_game->bulldog_altitude < 7) {
 //        my_game->lives--;
@@ -153,26 +197,27 @@ void loop() {
 //      }
 //    }
 //    my_game->bulldog_altitude = bulldog_jump_heights[cur_jumping_idx];
-//    
-//    fillLetters();
-//    fillBulldog();
-//    obstacle_cycle++;
-//    if (obstacle_cycle >= MAX_OBSTACLE_CYCLE) {
-//      obstacle_cycle = 0;
-//      obstacle_index++;
-//      // For now, restart the game when out of characters.
-//      if (obstacle_index >= MAX_OBSTACLE_INDEX) {
-//        obstacle_index = 0;
-//        my_game->lives = 4;
-//      }
-//    }
-//    
-////    Serial.write(12);
-////    printDisplayMat();
-////    Serial.println((char)char_under_bulldog);
-////    Serial.println(my_game->bulldog_jumping);
-//    previous_millis = current_millis;
-//  }
+    
+    fillLEDLetters();
+    fillLEDBulldog();
+    obstacle_cycle++;
+    if (obstacle_cycle >= MAX_OBSTACLE_CYCLE) {
+      obstacle_cycle = 0;
+      obstacle_index++;
+      // For now, restart the game when out of characters.
+      if (obstacle_index >= MAX_OBSTACLE_INDEX) {
+        obstacle_index = 0;
+        my_game->lives = 4;
+      }
+    }
+    
+//    Serial.write(12);
+//    printDisplayMat();
+//    Serial.println((char)char_under_bulldog);
+//    Serial.println(my_game->bulldog_jumping);
+    previous_millis = current_millis;
+    check_mem();
+  }
 }
 
 
@@ -211,17 +256,18 @@ void fillBulldog() {
 }
 
 void fillLEDBulldog() {
-  
+  unsigned char bulldog_char;
   for (int i = 0; i < BULLDOG_SIZE; i++) {
     for (int j = 0; j < BULLDOG_SIZE; j++) {
-      if (bulldog[i][j] != '_') {
-        if (bulldog[i][j] == 'W') {
-          color = matrix.Color333(7, 7, 7);
-        } else if (bulldog[i][j] == 'B') {
-          color = matrix.Color333(0, 0, 7);
-        }
-        matrix.drawPixel((BULLDOG_SIZE - 1) - i, BULLDOG_LEFT_OFFSET + j, color);
+      bulldog_char =  (unsigned char) pgm_read_byte(&(bulldog[i][j]));
+      if ( bulldog_char == '_') {
+        color = 0;
+      } else if ( bulldog_char == 'W') {
+        color = matrix.Color333(7, 7, 7);
+      } else if ( bulldog_char == 'B') {
+        color = matrix.Color333(0, 0, 7);
       }
+      matrix.drawPixel((BULLDOG_SIZE - 1) - i + my_game->bulldog_altitude, BULLDOG_LEFT_OFFSET + j, color);
     }
   }  
 }
@@ -241,7 +287,7 @@ void fillLetters() {
     if (current_column == 5) {
       continue;
     }
-    column_bitmap = Font5x7[((current_letter - 0x20) * 5) + current_column];
+    column_bitmap = 0;//Font5x7[((current_letter - 0x20) * 5) + current_column];
     for (int i = 0; i < 7; i++) {
       current_bit = column_bitmap & 0x1;
       column_bitmap >>= 1;
@@ -260,24 +306,23 @@ void fillLEDLetters() {
   int obstacle_top_offset = 25;
   color = matrix.Color333(7, 0, 7);
   for (int j = 0; j < 32; j++) {
-//    current_letter = obstacles[obstacle_index + (int) ((j + obstacle_cycle) / 6)];
-    current_letter = 'H';
-    if (current_letter == '_') {
-      continue;
-    }
+   // (unsigned char) pgm_read_byte(&(LetterFont[(('A' - 'A') * 5) + 0]));
+    current_letter =  (unsigned char) pgm_read_byte(&(obstacles[obstacle_index + (int) ((j + obstacle_cycle) / 6)]));
+    //current_letter = 'H';
     current_column = (j + obstacle_cycle) % 6;
-    if (current_column == 5) {
-      continue;
+    if (current_letter == '_' || current_column == 5) {
+      column_bitmap = 0;
+    } else {
+      column_bitmap = (unsigned char) pgm_read_byte(&(LetterFont[((current_letter - 'A') * 5) + current_column]));
     }
-    unsigned char temp_bitmap[] = {0x7E, 0x11, 0x11, 0x11, 0x7E};
-    column_bitmap = temp_bitmap[current_column];
-    Serial.print(column_bitmap);
+    //Serial.print(column_bitmap);
     for (int i = 0; i < 7; i++) {
       current_bit = column_bitmap & 0x1;
       column_bitmap >>= 1;
       if (current_bit == 0x1) {
         matrix.drawPixel((BULLDOG_SIZE - 1) - i, j, color);
-        
+      } else {
+        matrix.drawPixel((BULLDOG_SIZE - 1) - i, j, 0);
       }
     }
   }
