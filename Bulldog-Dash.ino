dbfeba32af0d8ab8713176b5c51eef9d6297dec0
thuynganvu <ngan.vu@yale.edu>
2019-04-25 19:08:25 -0400

    add lives counter display (but does not update lives yet)

--- Bulldog-Dash.ino
+++ Bulldog-Dash.ino
@@ -13,26 +13,31 @@
 #define C   A2
 #define D   A3
 
+// Sprites include bulldog and heart.
 #define SPRITE_HEIGHT (7)
+#define SPRITE_WIDTH (7)
+
+// Chars include letters and digits.
+#define CHAR_HEIGHT (7)
+#define CHAR_WIDTH (5)
 
 // For jumping direction.
 #define UP true
 #define DOWN false
 
 // For bulldog platform (where it is jumping from).
-#define OBSTACLE_TOP (SPRITE_HEIGHT)
+#define OBSTACLE_TOP (CHAR_HEIGHT)
 #define GROUND (0)
 
 RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false);
 
 uint16_t color;
 #define WHITE (matrix.Color333(7, 7, 7))
+#define RED (matrix.Color333(7, 0, 0))
 #define BLUE (matrix.Color333(0, 0, 7))
 #define PURPLE (matrix.Color333(7, 0, 7))
 
 #define DISP_SIZE (32)
-#define BULLDOG_SIZE (7)
-#define BULLDOG_FRONT_EAR_OFFSET (2)
 #define BULLDOG_LEFT_OFFSET (3)
 #define BULLDOG_FRONT_LEG_OFFSET (4)
 #define BULLDOG_BACK_LEG_OFFSET (2)
@@ -43,7 +48,6 @@ uint16_t color;
 int MAX_OBSTACLE_INDEX;
 
 typedef struct gameState {
-  char* disp_mat;
   int bulldog_altitude;
   int bulldog_max_altitude;
   int bulldog_platform;
@@ -54,7 +58,7 @@ typedef struct gameState {
 } gameState;
 
 // +1 because extra space needed for string null-terminator.
-const char bulldog[BULLDOG_SIZE][BULLDOG_SIZE + 1] PROGMEM = {
+const char bulldog[SPRITE_HEIGHT][SPRITE_WIDTH + 1] PROGMEM = {
   "__W___W",
   "___WWW_",
   "___WWW_",
@@ -64,7 +68,16 @@ const char bulldog[BULLDOG_SIZE][BULLDOG_SIZE + 1] PROGMEM = {
   "__W_W__"
 };
 
-// 130 bytes
+const char heart[SPRITE_HEIGHT][SPRITE_WIDTH + 1] PROGMEM = {
+  "_RR_RR_",
+  "RRRRRRR",
+  "RRRRRRR",
+  "RRRRRRR",
+  "_RRRRR_",
+  "__RRR__",
+  "___R___",
+};
+
 const unsigned char LetterFont[] PROGMEM = {
   0x7E, 0x11, 0x11, 0x11, 0x7E,// A
   0x7F, 0x49, 0x49, 0x49, 0x36,// B
@@ -92,6 +105,19 @@ const unsigned char LetterFont[] PROGMEM = {
   0x63, 0x14, 0x08, 0x14, 0x63,// X
   0x03, 0x04, 0x78, 0x04, 0x03,// Y
   0x61, 0x51, 0x49, 0x45, 0x43,// Z
+};
+
+const unsigned char DigitFont[] PROGMEM = {
+  0x3E, 0x51, 0x49, 0x45, 0x3E,// 0
+  0x00, 0x42, 0x7F, 0x40, 0x00,// 1
+  0x42, 0x61, 0x51, 0x49, 0x46,// 2
+  0x21, 0x41, 0x45, 0x4B, 0x31,// 3
+  0x18, 0x14, 0x12, 0x7F, 0x10,// 4
+  0x27, 0x45, 0x45, 0x45, 0x39,// 5
+  0x3C, 0x4A, 0x49, 0x49, 0x30,// 6
+  0x01, 0x71, 0x09, 0x05, 0x03,// 7
+  0x36, 0x49, 0x49, 0x49, 0x36,// 8
+  0x06, 0x49, 0x49, 0x29, 0x1E,// 9
 };
 
 const char obstacles[] PROGMEM =
@@ -153,7 +179,7 @@ void setup() {
   previous_millis = millis();
 
   matrix.fillScreen(0); // Clear the LED board.
-  displayLetters();
+  displayObstacles();
   displayBulldog();
 }
 
@@ -162,15 +188,15 @@ void loop() {
   if (current_millis - previous_millis >= delay_millis) {
     int read_val = Serial.read();
     
-    // When the letter 'A' is sent.
+    // When the char 'A' is sent.
     if (read_val == 'A' && my_game->bulldog_jumping == false) {
       my_game->bulldog_jumping = true;
       my_game->jumping_direction = UP;
     }
 
-    unsigned char front_leg_letter = getLetterOfColumn(BULLDOG_LEFT_OFFSET + BULLDOG_FRONT_LEG_OFFSET);
-    unsigned char back_leg_letter = getLetterOfColumn(BULLDOG_LEFT_OFFSET + BULLDOG_BACK_LEG_OFFSET);
-    bool bulldog_on_letter = (front_leg_letter != '_') && (back_leg_letter != '_');
+    unsigned char front_leg_char = getCharOfColumn(BULLDOG_LEFT_OFFSET + BULLDOG_FRONT_LEG_OFFSET);
+    unsigned char back_leg_char = getCharOfColumn(BULLDOG_LEFT_OFFSET + BULLDOG_BACK_LEG_OFFSET);
+    bool bulldog_on_char = (front_leg_char != '_') && (back_leg_char != '_');
 
     // Bulldog is jumping.
     if (my_game->bulldog_jumping == true) {
@@ -184,7 +210,7 @@ void loop() {
         clearBulldogRow(my_game->bulldog_platform + my_game->bulldog_altitude + (SPRITE_HEIGHT - 1));
         my_game->bulldog_altitude--;
         if (my_game->bulldog_platform == GROUND) {
-          if (my_game->bulldog_altitude == OBSTACLE_TOP && bulldog_on_letter) {
+          if (my_game->bulldog_altitude == OBSTACLE_TOP && bulldog_on_char) {
             my_game->bulldog_jumping = false;
             my_game->bulldog_platform = OBSTACLE_TOP;
             my_game->bulldog_altitude = 0;
@@ -203,7 +229,7 @@ void loop() {
 
     // Bulldog is not jumping.
     else {
-      if (my_game->bulldog_platform == OBSTACLE_TOP && !bulldog_on_letter) {
+      if (my_game->bulldog_platform == OBSTACLE_TOP && !bulldog_on_char) {
         my_game->bulldog_jumping = true;
         my_game->jumping_direction = DOWN;
         my_game->bulldog_platform = GROUND;
@@ -218,8 +244,9 @@ void loop() {
     Serial.print(my_game->bulldog_altitude);
     Serial.println(my_game->bulldog_platform + my_game->bulldog_altitude);
     
-    displayLetters();
+    displayObstacles();
     displayBulldog();
+    displayLives();
     obstacle_cycle++;
     if (obstacle_cycle >= MAX_OBSTACLE_CYCLE) {
       obstacle_cycle = 0;
@@ -235,39 +262,39 @@ void loop() {
   }
 }
 
-unsigned char getLetterOfColumn(int column) {
+unsigned char getCharOfColumn(int column) {
   return (unsigned char) pgm_read_byte(&(obstacles[obstacle_index + (int) ((column + obstacle_cycle) / 6)]));
 }
 
-unsigned char getLetterColumnOfColumn(int column) {
+unsigned char getCharColumnOfColumn(int column) {
   return (column + obstacle_cycle) % 6;
 }
 
 // Display the obstacles, except for things within the square containing the bulldog.
-void displayLetters() {
-  char current_letter;
-  int current_letter_column;
+void displayObstacles() {
+  unsigned char current_char;
+  int current_char_column;
   unsigned char current_column_bitmap;
   unsigned char current_bit;
   color = PURPLE;
   for (int col = 0; col < 32; col++) {
-    current_letter =  getLetterOfColumn(col);
-    current_letter_column = getLetterColumnOfColumn(col);
-    if (current_letter == '_' || current_letter_column == 5) {
+    current_char =  getCharOfColumn(col);
+    current_char_column = getCharColumnOfColumn(col);
+    if (current_char == '_' || current_char_column == 5) {
       current_column_bitmap = 0;
     } else {
-      current_column_bitmap = (unsigned char) pgm_read_byte(&(LetterFont[((current_letter - 'A') * 5) + current_letter_column]));
+      current_column_bitmap = (unsigned char) pgm_read_byte(&(LetterFont[((current_char - 'A') * 5) + current_char_column]));
     }
     for (int row = 0; row < 7; row++) {
       current_bit = current_column_bitmap & 0x1;
       current_column_bitmap >>= 1;
-      if (isInBulldogFrame((BULLDOG_SIZE - 1) - row, col)) {
+      if (isInBulldogFrame((CHAR_HEIGHT - 1) - row, col)) {
         continue;
       }
       if (current_bit == 0x1) {
-        matrix.drawPixel((BULLDOG_SIZE - 1) - row, col, color);
+        matrix.drawPixel((CHAR_HEIGHT - 1) - row, col, color);
       } else {
-        matrix.drawPixel((BULLDOG_SIZE - 1) - row, col, 0);
+        matrix.drawPixel((CHAR_HEIGHT - 1) - row, col, 0);
       }
     }
   }
@@ -277,28 +304,28 @@ void displayLetters() {
 
 // Display the bulldog, as well as anything that is within the square containing the bulldog.
 void displayBulldog() {
-  char current_letter;
-  int current_letter_column;
+  unsigned char current_char;
+  int current_char_column;
   unsigned char current_column_bitmap;
   unsigned char current_bit;
   unsigned char bulldog_pixel;
-  for (int col = 0; col < BULLDOG_SIZE; col++) {
-    current_letter =  getLetterOfColumn(BULLDOG_LEFT_OFFSET + col);
-    current_letter_column = getLetterColumnOfColumn(BULLDOG_LEFT_OFFSET + col);
-    if (current_letter == '_' || current_letter_column == 5) {
+  for (int col = 0; col < SPRITE_WIDTH; col++) {
+    current_char =  getCharOfColumn(BULLDOG_LEFT_OFFSET + col);
+    current_char_column = getCharColumnOfColumn(BULLDOG_LEFT_OFFSET + col);
+    if (current_char == '_' || current_char_column == 5) {
       current_column_bitmap = 0;
     } else {
-      current_column_bitmap = (unsigned char) pgm_read_byte(&(LetterFont[((current_letter - 'A') * 5) + current_letter_column]));
+      current_column_bitmap = (unsigned char) pgm_read_byte(&(LetterFont[((current_char - 'A') * 5) + current_char_column]));
     }
     
-    for (int row = 0; row < BULLDOG_SIZE; row++) {
+    for (int row = 0; row < SPRITE_HEIGHT; row++) {
       if (row >= (my_game->bulldog_platform + my_game->bulldog_altitude)) {
         current_bit = current_column_bitmap & 0x1;
         current_column_bitmap >>= 1;
       } else {
         current_bit = 0;
       }
-      bulldog_pixel =  (unsigned char) pgm_read_byte(&(bulldog[row][col]));
+      bulldog_pixel = (unsigned char) pgm_read_byte(&(bulldog[row][col]));
       if (bulldog_pixel == '_') {
         if (current_bit == 0x1) {
           color = PURPLE;
@@ -310,19 +337,53 @@ void displayBulldog() {
       } else if (bulldog_pixel == 'B') {
         color = BLUE;
       }
-      matrix.drawPixel((BULLDOG_SIZE - 1) - row + (my_game->bulldog_platform + my_game->bulldog_altitude), BULLDOG_LEFT_OFFSET + col, color);
+      matrix.drawPixel((SPRITE_HEIGHT - 1) - row + (my_game->bulldog_platform + my_game->bulldog_altitude), BULLDOG_LEFT_OFFSET + col, color);
     }
   }  
 }
 
 bool isInBulldogFrame(int row, int col) {
-  bool colInFrame = (col >= BULLDOG_LEFT_OFFSET && col < BULLDOG_LEFT_OFFSET + BULLDOG_SIZE);
+  bool colInFrame = (col >= BULLDOG_LEFT_OFFSET && col < BULLDOG_LEFT_OFFSET + SPRITE_WIDTH);
   bool rowInFrame = (row >= my_game->bulldog_platform + my_game->bulldog_altitude && row < my_game->bulldog_platform + my_game->bulldog_altitude + SPRITE_HEIGHT);
   return (colInFrame && rowInFrame);
 }
 
 void clearBulldogRow(int row) {
-  for (int col = 0; col < BULLDOG_SIZE; col++) {
+  for (int col = 0; col < SPRITE_WIDTH; col++) {
     matrix.drawPixel(row, BULLDOG_LEFT_OFFSET + col, 0);
+  }
+}
+
+void displayLives() {
+
+  // Draw number of lives.
+  unsigned char current_column_bitmap;
+  unsigned char current_bit;
+  for (int col = 0; col < CHAR_WIDTH; col++) {
+    current_column_bitmap = (unsigned char) pgm_read_byte(&(DigitFont[(my_game->lives * 5) + col]));
+    for (int row = 0; row < CHAR_HEIGHT; row++) {
+      current_bit = current_column_bitmap & 0x1;
+      current_column_bitmap >>= 1;
+      if (current_bit == 0x1) {
+        color = RED;
+      } else {
+        color = 0;
+      }
+      matrix.drawPixel((matrix.height() - 1) - row, col, color);
+    }
+  }
+
+  // Draw heart.
+  unsigned char heart_pixel;
+  for (int col = 0; col < SPRITE_WIDTH; col++) {
+    for (int row = 0; row < SPRITE_HEIGHT; row++) {
+      heart_pixel = (unsigned char) pgm_read_byte(&(heart[row][col]));
+      if (heart_pixel == '_') {
+        color = 0;
+      } else if (heart_pixel == 'R') {
+        color = RED;
+      }
+      matrix.drawPixel((matrix.height() - 1) - row, CHAR_WIDTH + 1 + col, color);
+    }
   }
 }
